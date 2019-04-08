14f817a88435adfbb8a8653706f8c14c8d1127d6
zgirmay <44984427+zgirmay@users.noreply.github.com>
2019-04-08 18:20:32 -0400

    Add files via upload

--- app.py
+++ app.py
@@ -3,14 +3,16 @@ import pandas as pd
 from flask import (
     Flask,
     render_template,
-    jsonify)
+    jsonify,
+    request)
 
 from flask_sqlalchemy import SQLAlchemy
+
 
 app = Flask(__name__)
 
 # The database URI
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///db/Volcanos.sqlite.db"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///Volcanos.sqlite.db"
 
 db = SQLAlchemy(app)
 
@@ -21,8 +23,8 @@ class Volcanos(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String)
-    latitude = db.Column(db.Decimal)
-    longitude = db.Column(db.Decimal)
+    latitude = db.Column(db.Integer)
+    longitude = db.Column(db.Integer)
     active = db.Column(db.String)
     explosive = db.Column(db.Integer)
     startyear = db.Column(db.Integer)
@@ -46,6 +48,69 @@ def setup():
 def home():
     """Render Home Page."""
     return render_template("index.html")
+
+@app.route("/volcanos_explosive")
+def volcanos_explosive_data():
+    """Return volcano explosive rating and volcano name"""
+
+    # Query for the volcano name/explosive data
+    results = db.session.query(Volcano_Erruptions.name, Volcano_Erruptions.explosive).\
+        order_by(Volcano_Erruptions.explosive.desc())
+    df = pd.DataFrame(results, columns=['name', 'explosive'])
+
+    # Format the data for Plotly
+    plot_trace = {
+        "x": df["name"].values.tolist(),
+        "y": df["explosive"].values.tolist(),
+        "type": "bar"
+    }
+    return jsonify(plot_trace)
+
+@app.route("/volcanos_explosive")
+def volcanos_explosive_data():
+    """Return volcano explosive rating and volcano name"""
+
+    # Query for the volcano name/explosive data
+    results = db.session.query(Volcano_Erruptions.name, Volcano_Erruptions.explosive).\
+        order_by(Volcano_Erruptions.explosive.desc())
+    df = pd.DataFrame(results, columns=['name', 'explosive'])
+
+    # Format the data for Plotly
+    plot_trace = {
+        "x": df["name"].values.tolist(),
+        "y": df["explosive"].values.tolist(),
+        "type": "bar"
+    }
+    return jsonify(plot_trace)
+
+ @app.route('/form')
+def form():
+  longitude = request.args.get('longitude', type=float)
+  latitude = request.args.get('latitude', type=float)
+
+  return render_template("Visualization-AG.html", longitude=longitude, latitude=latitude)   
+
+@app.route("/scatter")
+def volcano_scatter_data():
+    """Return volcano name and volcano explosive/startyear"""
+
+    # Query for the top 10 emoji data
+    results = db.session.query(volcano_scatter_data.name, volcano_scatter_data.explosive, volcano_scatter_data.startyear).\
+        order_by(volcano_scatter_data.startyear.desc()).
+
+    # Create lists from the query results
+    volcano_names = [result[0] for result in results]
+    explosivity = [int(result[1]) for result in results]
+    year = [int(result[2]) for result in results]
+    # Generate the plot trace
+    trace = {
+        "x": volcano_names,
+        "y": explosivity,
+        "type": "scatter"
+    }
+    return jsonify(trace)
+
+
 
 
 
